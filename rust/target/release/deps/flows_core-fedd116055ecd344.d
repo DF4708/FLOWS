f62b6e88dfd0431a9adcb274d651577b7264acee
DF4708 <138391381+DF4708@users.noreply.github.com>
2026-07-23 04:13:34 -0400

    Rebuild flows-core with transit module support
    Update build artifacts for flows-core library to include new transit modules (mod, ftt, gtfs, raptor). This reflects the addition of transit-related functionality to the routing core.

--- rust/target/release/deps/flows_core-fedd116055ecd344.d
+++ rust/target/release/deps/flows_core-fedd116055ecd344.d
@@ -1,6 +1,6 @@
-/Users/soulary/Documents/Coding_Files/FLOWS/rust/target/release/deps/flows_core-fedd116055ecd344.d: flows-core/src/lib.rs flows-core/src/ch.rs flows-core/src/distance.rs flows-core/src/ffi.rs flows-core/src/polyline.rs flows-core/src/risk.rs flows-core/src/routing.rs flows-core/src/scoring.rs
+/Users/soulary/Documents/Coding_Files/FLOWS/rust/target/release/deps/flows_core-fedd116055ecd344.d: flows-core/src/lib.rs flows-core/src/ch.rs flows-core/src/distance.rs flows-core/src/ffi.rs flows-core/src/polyline.rs flows-core/src/risk.rs flows-core/src/routing.rs flows-core/src/scoring.rs flows-core/src/transit/mod.rs flows-core/src/transit/ftt.rs flows-core/src/transit/gtfs.rs flows-core/src/transit/raptor.rs
 
-/Users/soulary/Documents/Coding_Files/FLOWS/rust/target/release/deps/flows_core-fedd116055ecd344: flows-core/src/lib.rs flows-core/src/ch.rs flows-core/src/distance.rs flows-core/src/ffi.rs flows-core/src/polyline.rs flows-core/src/risk.rs flows-core/src/routing.rs flows-core/src/scoring.rs
+/Users/soulary/Documents/Coding_Files/FLOWS/rust/target/release/deps/flows_core-fedd116055ecd344: flows-core/src/lib.rs flows-core/src/ch.rs flows-core/src/distance.rs flows-core/src/ffi.rs flows-core/src/polyline.rs flows-core/src/risk.rs flows-core/src/routing.rs flows-core/src/scoring.rs flows-core/src/transit/mod.rs flows-core/src/transit/ftt.rs flows-core/src/transit/gtfs.rs flows-core/src/transit/raptor.rs
 
 flows-core/src/lib.rs:
 flows-core/src/ch.rs:
@@ -10,3 +10,7 @@ flows-core/src/polyline.rs:
 flows-core/src/risk.rs:
 flows-core/src/routing.rs:
 flows-core/src/scoring.rs:
+flows-core/src/transit/mod.rs:
+flows-core/src/transit/ftt.rs:
+flows-core/src/transit/gtfs.rs:
+flows-core/src/transit/raptor.rs:
